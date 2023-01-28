d761462f79f88a83640995b52b389847ccd99840
Kelso <oslek@hotmail.com>
2023-01-28 16:22:15 -0500

    Added Motor IDs

--- Robot-2023/src/main/java/frc/robot/Constants.java
+++ Robot-2023/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final int ARM_MOTOR_CAN_ID = 6;
+    public static final int LEFT_INTAKE_MOTOR_CAN_ID = 7;
+    public static final int RIGHT_INTAKE_MOTOR_CAN_ID = 8;
+
+
+
+}
