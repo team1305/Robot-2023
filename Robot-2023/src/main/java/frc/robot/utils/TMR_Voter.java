1ebf1643c5a4464665bf7efbd3299a1937233df3
Kelso <oslek@hotmail.com>
2023-01-29 18:14:42 -0500

    Created a balance auto and all of its dependencies

---
+++ Robot-2023/src/main/java/frc/robot/utils/TMR_Voter.java
@@ -0,0 +1,11 @@
+package frc.robot.utils;
+
+public class TMR_Voter {
+    public TMR_Voter(
+        double value1,
+        double value2,
+        double... valueN
+        ){
+            
+        }
+}
